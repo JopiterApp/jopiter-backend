8490db89cd55eaaa9a34ad37a86172f468b998c1
nkzren <renan.nakazawa0@gmail.com>
2022-03-31 09:32:07 -0300

    Remove JMX

--- docker/resources/entrypoint.sh
+++ docker/resources/entrypoint.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
 set -e
 
-JMX_OPTS="${JMX_OPTS:--XX:+UnlockCommercialFeatures -XX:+FlightRecorder -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.port=9090}"
-
 exec java \
           $APPLICATION_HEAP_OPTS \
-          $APPLICATION_JMX_OPTS \
           $APPLICATION_JAVA_OPTS \
           -jar "$APPLICATION_HOME/app.jar" \
           $*
