ff4e318ee52809d9ddd9e24e1f0e0dd0c5e268b2
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2024-03-04 19:14:13 -0300

    🔖 Prepare release 2.3.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
-version = "2.3.8"
+version = "2.3.9"
 group = "app.jopiter"
 
 plugins {
